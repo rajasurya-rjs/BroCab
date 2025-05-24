edf0538658372c8aa9a2c91a6aaf7b40e4a1f105
Bhuvanesh66 <bhuvaneshms60@gamil.com>
2025-05-25 00:31:02 +0530

    df

--- dataconnect-generated/js/default-connector/esm/index.esm.js
+++
@@ -1,7 +0,0 @@
-
-export const connectorConfig = {
-  connector: 'default',
-  service: 'brocab',
-  location: 'us-central1'
-};
-
--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,7 +0,0 @@
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'brocab',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
